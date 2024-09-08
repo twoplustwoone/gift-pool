bde0ead1139bc7266cbfec80fa4b1d4e4f46c648
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-09-08 01:28:43 -0400

    fix: update user search to use wishlist item instead of note

--- app/routes/users+/index.tsx
+++ app/routes/users+/index.tsx
@@ -30,10 +30,10 @@ export async function loader({ request }: LoaderFunctionArgs) {
 		WHERE User.username LIKE ${like}
 		OR User.name LIKE ${like}
 		ORDER BY (
-			SELECT Note.updatedAt
-			FROM Note
-			WHERE Note.ownerId = User.id
-			ORDER BY Note.updatedAt DESC
+			SELECT WishlistItem.updatedAt
+			FROM WishlistItem
+			WHERE WishlistItem.ownerId = User.id
+			ORDER BY WishlistItem.updatedAt DESC
 			LIMIT 1
 		) DESC
 		LIMIT 50
