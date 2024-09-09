8c5da3c747f5893a24d56860938d3352a7c06be2
Francisco D <francisco.digiandomenico@gmail.com>
2024-09-08 20:23:29 -0400

    Update email sender address to 'hello@giftpool.app' (#2)
    * chore: update email sender address to 'hello@giftpool.app'
    
    * chore: update email sender address to 'hello@giftpool.app'

--- tests/utils.ts
+++ tests/utils.ts
@@ -2,7 +2,7 @@ import * as setCookieParser from 'set-cookie-parser'
 import { sessionKey } from '#app/utils/auth.server.ts'
 import { authSessionStorage } from '#app/utils/session.server.ts'
 
-export const BASE_URL = 'https://www.epicstack.dev'
+export const BASE_URL = 'https://www.giftpool.app'
 
 export function convertSetCookieToCookie(setCookie: string) {
 	const parsedCookie = setCookieParser.parseString(setCookie)
