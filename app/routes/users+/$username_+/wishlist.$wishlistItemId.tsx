484448b89fcc75627bef02a0eb47c8a87029666d
Francisco D <francisco.digiandomenico@gmail.com>
2024-09-10 17:50:50 +0000

    fix: add some perms and validations for deletion

--- app/routes/users+/$username_+/wishlist.$wishlistItemId.tsx
+++ app/routes/users+/$username_+/wishlist.$wishlistItemId.tsx
@@ -1,12 +1,45 @@
+import { parse } from '@conform-to/zod'
+import { invariantResponse } from '@epic-web/invariant'
 import { json, type ActionFunctionArgs } from '@remix-run/node'
 import { prisma } from '#app/utils/db.server.ts'
+import { requireUserId } from '#app/utils/auth.server.js'
+import { validateCSRF } from '#app/utils/csrf.server.js'
+import { z } from 'zod'
+import { requireUserWithPermission } from '#app/utils/permissions.js'
 
-export async function action({ params }: ActionFunctionArgs) {
-	const { wishlistItemId } = params
+const DeleteFormSchema = z.object({
+	intent: z.literal('delete-note'),
+	noteId: z.string(),
+})
+
+export async function action({ request }: ActionFunctionArgs) {
+	const userId = await requireUserId(request)
+	const formData = await request.formData()
+	await validateCSRF(formData, request.headers)
+	const submission = parse(formData, {
+		schema: DeleteFormSchema,
+	})
+
+	const { wishlistItemId } = submission.value
 
 	await prisma.wishlistItem.delete({
 		where: { id: wishlistItemId },
 	})
+
+	const wishlistItem = await prisma.wishlistItem.findFirst({
+		select: { id: true, ownerId: true, owner: { select: { username: true } } },
+		where: { id: wishlistItemId },
+	})
+
+	invariantResponse(wishlistItem, 'Not found', { status: 404 })
+
+	const isOwner = wishlistItem.ownerId === userId
+	await requireUserWithPermission(
+		request,
+		isOwner ? `delete:note:own` : `delete:note:any`,
+	)
+
+	await prisma.wishlistItem.delete({ where: { id: wishlistItem.id } })
 
 	return json({ success: true, deletedItemId: wishlistItemId })
 }
