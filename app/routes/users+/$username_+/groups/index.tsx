34aeb85c48d95f8bdf7fd8b8305821d04971c311
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-09-14 16:39:57 -0400

    feat(groups): add user authentication to route

--- app/routes/users+/$username_+/groups/index.tsx
+++ app/routes/users+/$username_+/groups/index.tsx
@@ -1,3 +1,11 @@
-export default function Groups() {
+import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import { requireUserId } from '#app/utils/auth.server.ts'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requireUserId(request)
+	return json({})
+}
+
+export default function Groups({}) {
 	return <div></div>
 }
