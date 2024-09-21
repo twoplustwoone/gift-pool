603bf12fcf3a2ccf193b7f8a09e5d613b4264b8a
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-09-21 18:52:11 -0400

    chore: something

--- app/routes/users+/$username_+/route.tsx
+++ app/routes/users+/$username_+/route.tsx
@@ -22,7 +22,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
 export default function Username() {
 	return (
 		<main className="container flex h-full min-h-[400px] px-0 pb-12 md:px-8">
-			<div className="grid w-full bg-muted pl-2 md:container md:rounded-3xl md:pr-0">
+			<div className="grid w-full bg-muted pl-2 pr-2 md:container md:rounded-3xl">
 				<Outlet />
 			</div>
 		</main>
