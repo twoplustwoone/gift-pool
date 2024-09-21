097048ee362a8c011e33d3f2b978f6b85b9750b5
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-09-21 18:51:52 -0400

    fix(index): clean up

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,13 +7,9 @@ export default function Index() {
 		<main className="font-poppins grid h-full place-items-center">
 			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
 				<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
-					<a
-						href="https://www.epicweb.dev/stack"
-						className="animate-slide-top [animation-fill-mode:backwards] xl:animate-slide-left xl:[animation-delay:0.5s] xl:[animation-fill-mode:backwards]"
-					></a>
 					<h1
 						data-heading
-						className="text-10xl mt-8 animate-slide-top text-foreground [animation-fill-mode:backwards] [animation-delay:0.3s] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-fill-mode:backwards] xl:[animation-delay:0.8s]"
+						className="text-10xl mt-8 animate-slide-top text-foreground [animation-delay:0.3s] [animation-fill-mode:backwards] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-delay:0.8s] xl:[animation-fill-mode:backwards]"
 					>
 						<span className="font-light">gift</span>
 						<span className="font-bold">pool</span>
