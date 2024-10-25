02111f4180634a120298a0918a93f12a2d63dbe5
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-10-24 20:40:30 -0400

    feat(groups): create and destroy invite links

--- app/routes/groups+/$giftGroupId_+/index.tsx
+++ app/routes/groups+/$giftGroupId_+/index.tsx
@@ -1,12 +1,17 @@
-import { getFormProps, useForm } from '@conform-to/react'
+import { getFormProps, useForm, useInputControl } from '@conform-to/react'
 import { getZodConstraint, parseWithZod } from '@conform-to/zod'
+import { type GroupInvitation } from '@prisma/client'
 import {
 	type ActionFunctionArgs,
 	json,
 	type LoaderFunctionArgs,
 } from '@remix-run/node'
-import { Form, useActionData, useLoaderData } from '@remix-run/react'
-import { nanoid } from 'nanoid'
+import {
+	Form,
+	useActionData,
+	useFetcher,
+	useLoaderData,
+} from '@remix-run/react'
 import { useState } from 'react'
 import { AuthenticityTokenInput } from 'remix-utils/csrf/react'
 
@@ -49,18 +54,28 @@ import {
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { validateCSRF } from '#app/utils/csrf.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
-import { getInviteLink } from '#app/utils/group-invitations.server.ts'
 import {
-	requireUserInGroup,
-	requireUserWithGroupPermission,
-	userHasGroupPermission,
-} from '#app/utils/group-permissions.server.ts'
+	createInviteLink,
+	destroyInviteLink,
+	getInviteLink,
+} from '#app/utils/group-invitations.server.ts'
+import { userHasGroupPermission } from '#app/utils/group-permissions.server.ts'
+import {
+	deleteGiftGroup,
+	leaveGroup,
+	requireUserIdInGroup,
+} from '#app/utils/groups.server.ts'
 import { useDebounce, useIsPending } from '#app/utils/misc.tsx'
-import { redirectWithToast } from '#app/utils/toast.server.ts'
+import {
+	createToastHeaders,
+	redirectWithToast,
+} from '#app/utils/toast.server.ts'
 
 export enum GiftGroupIdFormIntent {
 	DeleteGiftGroup = 'delete-gift-group',
 	CreateInviteLink = 'create-invite-link',
+	DestroyInviteLink = 'destroy-invite-link',
+	LeaveGiftGroup = 'leave-gift-group',
 }
 
 const DeleteFormSchema = z.object({
@@ -74,9 +89,20 @@ export const CreateInviteLinkFormSchema = z.object({
 	expiresInDays: z.string(),
 })
 
+export const DestroyInviteLinkFormSchema = z.object({
+	intent: z.literal(GiftGroupIdFormIntent.DestroyInviteLink),
+	giftGroupId: z.string(),
+	groupInvitationId: z.string(),
+})
+
+export const LeaveGroupFormSchema = z.object({
+	intent: z.literal(GiftGroupIdFormIntent.LeaveGiftGroup),
+	giftGroupId: z.string(),
+})
+
 export async function loader({ params, request }: LoaderFunctionArgs) {
 	const groupId = params.giftGroupId!
-	const userId = await requireUserInGroup(request, groupId)
+	const userId = await requireUserIdInGroup(request, groupId)
 
 	const giftGroup = await prisma.giftGroup.findUnique({
 		where: { id: groupId },
@@ -111,11 +137,11 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 
 	const canDelete = await userHasGroupPermission(userId, groupId, 'deleteGroup')
 	const canInvite = await userHasGroupPermission(userId, groupId, 'addMember')
+	const canLeave = await userHasGroupPermission(userId, groupId, 'leaveGroup')
 
-	// Fetch existing invitation link if it exists
-	let inviteLink: string | null = null
+	let existingInvitation: GroupInvitation | null = null
 	if (canInvite) {
-		const existingInvitation = await prisma.groupInvitation.findFirst({
+		existingInvitation = await prisma.groupInvitation.findFirst({
 			where: {
 				giftGroupId: groupId,
 				expiresAt: { gt: new Date() },
@@ -124,13 +150,18 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 				createdAt: 'desc',
 			},
 		})
-
-		if (existingInvitation) {
-			inviteLink = getInviteLink(existingInvitation.code)
-		}
 	}
 
-	return json({ giftGroup, canInvite, canDelete, inviteLink })
+	return json({
+		giftGroup,
+		canInvite,
+		canDelete,
+		canLeave,
+		inviteLink: existingInvitation
+			? getInviteLink(existingInvitation.code)
+			: null,
+		groupInvitationId: existingInvitation?.id,
+	})
 }
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -138,120 +169,60 @@ export async function action({ request }: ActionFunctionArgs) {
 	const formData = await request.formData()
 	await validateCSRF(formData, request.headers)
 
-	const formIntent = formData.get('intent')
+	const submission = parseWithZod(formData, {
+		schema: DeleteFormSchema.or(CreateInviteLinkFormSchema)
+			.or(DestroyInviteLinkFormSchema)
+			.or(LeaveGroupFormSchema),
+	})
 
-	let submission
-	let giftGroupId
-	switch (formIntent) {
+	if (submission.status !== 'success') {
+		return json(submission.reply(), {
+			status: submission.status === 'error' ? 400 : 200,
+		})
+	}
+
+	const { giftGroupId } = submission.value
+
+	switch (submission.value.intent) {
 		case GiftGroupIdFormIntent.DeleteGiftGroup:
-			submission = parseWithZod(formData, {
-				schema: DeleteFormSchema,
-			})
-
-			if (submission.status !== 'success') {
-				return json(submission.reply(), {
-					status: submission.status === 'error' ? 400 : 200,
-				})
-			}
-
-			// if (submission.intent !== 'submit') {
-			// 	return json({
-			// 		intent: GiftGroupIdFormIntent.DeleteGiftGroup,
-			// 		status: 'idle',
-			// 		submission,
-			// 	})
-			// }
-			// if (!submission.value) {
-			// 	return json(
-			// 		{
-			// 			intent: GiftGroupIdFormIntent.DeleteGiftGroup,
-			// 			status: 'error',
-			// 			submission,
-			// 		},
-			// 		{ status: 400 },
-			// 	)
-			// }
-
-			giftGroupId = submission.value.giftGroupId
-
-			await requireUserWithGroupPermission(request, giftGroupId, 'deleteGroup')
-
-			await prisma.giftGroup.delete({ where: { id: giftGroupId } })
-
-			return redirectWithToast(`/groups`, {
+			await deleteGiftGroup(request, submission.value)
+			return redirectWithToast('/groups', {
 				type: 'success',
 				title: 'Success',
-				description: `Group has been deleted.`,
+				description: 'Group has been deleted.',
 			})
+
 		case GiftGroupIdFormIntent.CreateInviteLink:
-			submission = parseWithZod(formData, {
-				schema: CreateInviteLinkFormSchema,
+			await createInviteLink(request, submission.value)
+			return json(submission.reply(), {
+				headers: await createToastHeaders({
+					description: 'Invite link has been created.',
+					type: 'success',
+				}),
 			})
 
-			if (submission.status !== 'success') {
-				return json(submission.reply(), {
-					status: submission.status === 'error' ? 400 : 200,
-				})
-			}
-
-			// if (submission.intent !== 'submit') {
-			// 	return json({
-			// 		status: 'idle',
-			// 		submission,
-			// 		intent: GiftGroupIdFormIntent.CreateInviteLink,
-			// 	})
-			// }
-			// if (!submission.value) {
-			// 	return json(
-			// 		{
-			// 			status: 'error',
-			// 			submission,
-			// 			intent: GiftGroupIdFormIntent.CreateInviteLink,
-			// 		},
-			// 		{ status: 400 },
-			// 	)
-			// }
-
-			const { expiresInDays } = submission.value
-			giftGroupId = submission.value.giftGroupId
-
-			const expiresAt = new Date()
-			expiresAt.setDate(expiresAt.getDate() + parseInt(expiresInDays, 10))
-
-			const userId = await requireUserWithGroupPermission(
-				request,
-				giftGroupId,
-				'addMember',
-			)
-
-			await prisma.groupInvitation.create({
-				data: {
-					giftGroupId,
-					code: nanoid(),
-					expiresAt,
-					createdById: userId,
-				},
+		case GiftGroupIdFormIntent.DestroyInviteLink:
+			await destroyInviteLink(request, giftGroupId, submission.value)
+			return json(submission.reply(), {
+				headers: await createToastHeaders({
+					description: 'Invite link has been destroyed.',
+					type: 'success',
+				}),
 			})
 
-			// TODO: need to load this instead of returning it from action maybe?
-			// const inviteLink = getInviteLink(invitation.code)
+		case GiftGroupIdFormIntent.LeaveGiftGroup:
+			await leaveGroup(request, giftGroupId)
 
-			return json(
-				submission.reply(),
-				// {
-				// status: 'success',
-				// inviteLink,
-				// submission,
-				// intent: GiftGroupIdFormIntent.CreateInviteLink,
-				// }
-			)
-		default:
-			throw new Error(`Unknown form intent: ${formIntent}`)
+			return redirectWithToast('/groups', {
+				type: 'success',
+				title: 'Success',
+				description: 'You have left the group.',
+			})
 	}
 }
 
 export default function GiftGroupIndex() {
-	const { giftGroup, canInvite, canDelete, inviteLink } =
+	const { giftGroup, canInvite, canDelete, canLeave } =
 		useLoaderData<typeof loader>()
 
 	return (
@@ -259,15 +230,13 @@ export default function GiftGroupIndex() {
 			<SectionTitle>
 				<div className="flex min-h-10 w-full content-between justify-between">
 					<Heading>{giftGroup.name}</Heading>
-					<div className="flex gap-1">
-						{canInvite && (
-							<CreateInviteLinkDialog
-								giftGroupId={giftGroup.id}
-								link={inviteLink}
-							/>
-						)}
-						{canDelete && <DeleteGroupDialog id={giftGroup.id} />}
-					</div>
+					{(canInvite || canDelete || canLeave) && (
+						<div className="flex gap-1">
+							{canInvite && <CreateInviteLinkDialog />}
+							{canDelete && <DeleteGroupDialog id={giftGroup.id} />}
+							{canLeave && <LeaveGroupDialog id={giftGroup.id} />}
+						</div>
+					)}
 				</div>
 				<SectionSubtitle>
 					<Subheading>{giftGroup.description}</Subheading>
@@ -291,43 +260,32 @@ export default function GiftGroupIndex() {
 	)
 }
 
-function CreateInviteLinkDialog({
-	link: initialLink,
-	giftGroupId,
-}: {
-	link: string | null
-	giftGroupId: string
-}) {
+function CreateInviteLinkDialog() {
+	const { inviteLink, giftGroup } = useLoaderData<typeof loader>()
 	const actionData = useActionData<typeof action>()
 	const isPending = useIsPending()
-	const [form] = useForm({
+	const [form, fields] = useForm({
 		id: GiftGroupIdFormIntent.CreateInviteLink,
 		lastResult: actionData,
 		constraint: getZodConstraint(CreateInviteLinkFormSchema),
-		// onValidate({ formData }) {
-		// 	return parseWithZod(formData, { schema: CreateInviteLinkFormSchema })
-		// },
+		onValidate({ formData }) {
+			return parseWithZod(formData, { schema: CreateInviteLinkFormSchema })
+		},
 		defaultValue: {
 			expiresInDays: '7',
 		},
 	})
 
-	const [link /* , setLink */] = useState(initialLink)
-	const [hasCopied, setHasCopied] = useState(false)
+	const expiresInDays = useInputControl(fields.expiresInDays)
 
-	// TODO: load from loader
-	// useEffect(() => {
-	// 	if (actionData?.inviteLink) {
-	// 		setLink(actionData.inviteLink)
-	// 	}
-	// }, [actionData])
+	const [hasCopied, setHasCopied] = useState(false)
 
 	const debouncedReset = useDebounce(() => setHasCopied(false), 2000)
 
 	const copyLink = () => {
 		setHasCopied(true)
 		debouncedReset()
-		void navigator.clipboard.writeText(link!)
+		void navigator.clipboard.writeText(inviteLink!)
 	}
 
 	const handleInputClick = (
@@ -354,7 +312,7 @@ function CreateInviteLinkDialog({
 					</DialogDescription>
 				</DialogHeader>
 				<div className="grid gap-4 py-4">
-					{link ? (
+					{inviteLink ? (
 						<div className="flex items-center space-x-2">
 							<div className="grid flex-1 gap-2">
 								<Label htmlFor="link" className="sr-only">
@@ -362,7 +320,7 @@ function CreateInviteLinkDialog({
 								</Label>
 								<Input
 									id="link"
-									defaultValue={link}
+									defaultValue={inviteLink}
 									readOnly
 									onClick={handleInputClick}
 								/>
@@ -382,21 +340,24 @@ function CreateInviteLinkDialog({
 					) : (
 						<Form method="POST" {...getFormProps(form)}>
 							<AuthenticityTokenInput />
-							<input type="hidden" name="giftGroupId" value={giftGroupId} />
+							<input type="hidden" name="giftGroupId" value={giftGroup.id} />
 							<div className="flex items-center">
 								<div className="w-1/2">Link expires after</div>
 								<div className="w-1/2">
 									<Select
-										defaultValue="7"
-										onValueChange={() => {
-											// TODO: hook up change to form
-										}}
+										value={expiresInDays.value}
+										onValueChange={expiresInDays.change}
+										defaultValue={fields.expiresInDays.initialValue}
+										name={fields.expiresInDays.name}
 									>
 										<SelectTrigger>
 											<SelectValue placeholder="Select an expiration time" />
 										</SelectTrigger>
 										<SelectContent>
-											<SelectGroup>
+											<SelectGroup
+												onFocus={expiresInDays.focus}
+												onBlur={expiresInDays.blur}
+											>
 												<SelectItem value="1">1 day</SelectItem>
 												<SelectItem value="3">3 days</SelectItem>
 												<SelectItem value="7">7 days</SelectItem>
@@ -415,16 +376,8 @@ function CreateInviteLinkDialog({
 								Cancel
 							</Button>
 						</DialogClose>
-						{/* <input type="hidden" name="giftGroupId" value={id} /> */}
-						{link ? (
-							<StatusButton
-								status={isPending ? 'pending' : (actionData?.status ?? 'idle')}
-								variant="destructive"
-							>
-								{/* Probably need a better label than this */}
-								{/* On click, will invalidate the code in the backend */}
-								Destroy link
-							</StatusButton>
+						{inviteLink ? (
+							<DestroyInviteLinkButton />
 						) : (
 							<StatusButton
 								type="submit"
@@ -444,6 +397,36 @@ function CreateInviteLinkDialog({
 				</div>
 			</DialogContent>
 		</Dialog>
+	)
+}
+
+function DestroyInviteLinkButton() {
+	const { giftGroup, groupInvitationId } = useLoaderData<typeof loader>()
+	const fetcher = useFetcher<typeof action>()
+	const [form] = useForm({
+		id: GiftGroupIdFormIntent.DestroyInviteLink,
+		lastResult: fetcher.data,
+		constraint: getZodConstraint(DestroyInviteLinkFormSchema),
+	})
+	return (
+		<fetcher.Form method="POST" {...getFormProps(form)}>
+			<AuthenticityTokenInput />
+			<input type="hidden" name="giftGroupId" value={giftGroup.id} />
+			<input type="hidden" name="groupInvitationId" value={groupInvitationId} />
+			<StatusButton
+				status={
+					fetcher.state === 'submitting' ? 'pending' : (form.status ?? 'idle')
+				}
+				disabled={fetcher.state !== 'idle'}
+				value={GiftGroupIdFormIntent.DestroyInviteLink}
+				form={form.id}
+				variant="destructive"
+				type="submit"
+				name="intent"
+			>
+				Destroy link
+			</StatusButton>
+		</fetcher.Form>
 	)
 }
 
@@ -495,6 +478,57 @@ function DeleteGroupDialog({ id }: { id: string }) {
 							Delete group
 						</StatusButton>
 						<ErrorList errors={form.errors} id={form.errorId} />
+					</Form>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
+}
+
+function LeaveGroupDialog({ id }: { id: string }) {
+	const actionData = useActionData<typeof action>()
+	const isPending = useIsPending()
+	const [form] = useForm({
+		id: GiftGroupIdFormIntent.LeaveGiftGroup,
+		lastResult: actionData,
+	})
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button variant={'destructive'}>
+					<Icon name="exit" className="scale-125 max-md:scale-150">
+						<span className="max-md:hidden">Leave group</span>
+					</Icon>
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Leave group</DialogTitle>
+					<DialogDescription>
+						Are you sure you want to leave this group?
+					</DialogDescription>
+				</DialogHeader>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant={'secondary'} type="button">
+							Cancel
+						</Button>
+					</DialogClose>
+					<Form method="POST" {...getFormProps(form)}>
+						<AuthenticityTokenInput />
+						<input type="hidden" name="giftGroupId" value={id} />
+						<StatusButton
+							type="submit"
+							name="intent"
+							value={GiftGroupIdFormIntent.LeaveGiftGroup}
+							variant="destructive"
+							status={isPending ? 'pending' : (actionData?.status ?? 'idle')}
+							disabled={isPending}
+							className="w-full max-md:aspect-square max-md:px-0"
+						>
+							Leave group
+						</StatusButton>
 					</Form>
 				</DialogFooter>
 			</DialogContent>
