0896f80d018da5cf747e30461699a8608d060875
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-10-27 19:54:21 -0400

    fix(users): add group checking to route page

--- app/routes/users+/$username_+/route.tsx
+++ app/routes/users+/$username_+/route.tsx
@@ -1,22 +1,34 @@
 import { invariantResponse } from '@epic-web/invariant'
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { Outlet } from '@remix-run/react'
+import { Outlet, redirect } from '@remix-run/react'
+import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
+import { requireUsersShareAGroup } from '#app/utils/groups.server.ts'
 
-export async function loader({ params }: LoaderFunctionArgs) {
-	const owner = await prisma.user.findFirst({
+export async function loader({ params, request }: LoaderFunctionArgs) {
+	const { username } = params
+
+	const userId = await requireUserId(request)
+	await requireUsersShareAGroup({ userId, username })
+
+	const user = await prisma.user.findFirst({
 		select: {
 			id: true,
 			name: true,
 			username: true,
+			wishlistItems: { select: { id: true, value: true, ownerId: true } },
 			image: { select: { id: true } },
 		},
-		where: { username: params.username },
+		where: { username },
 	})
 
-	invariantResponse(owner, 'Owner not found', { status: 404 })
+	invariantResponse(user, 'User not found', { status: 404 })
 
-	return json({ owner })
+	if (user.id === userId) {
+		return redirect('/me')
+	}
+
+	return json({})
 }
 
 export default function Username() {
