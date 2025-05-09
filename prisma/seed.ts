bc45a8f4c14a19564987064271c49ce6d573908f
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2025-05-09 15:28:12 -0700

    Remove unused import of promiseHash from seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
 import { faker } from '@faker-js/faker'
-import { promiseHash } from 'remix-utils/promise'
 import { prisma } from '#app/utils/db.server.ts'
 import { MOCK_CODE_GITHUB } from '#app/utils/providers/constants'
 import {
