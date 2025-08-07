d9f5b973ff1796152d8a9475a743f9fbef360ed4
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2025-08-07 19:20:44 -0400

    fix: update password field selector in 2FA e2e test

--- tests/e2e/2fa.test.ts
+++ tests/e2e/2fa.test.ts
@@ -37,7 +37,7 @@ test('Users can add 2FA to their account and use it when logging in', async ({
   await page.goto('/login');
   await expect(page).toHaveURL(`/login`);
   await page.getByRole('textbox', { name: /username/i }).fill(user.username);
-  await page.getByLabel(/^password$/i).fill(password);
+  await page.getByRole('textbox', { name: /password/i }).fill(password);
   await page.getByRole('button', { name: /log in/i }).click();
 
   await page
