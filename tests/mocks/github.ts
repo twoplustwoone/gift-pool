c123abf613cd01141c567fef25ebca9979b8594c
Francisco <francisco.digiandomenico@gmail.com>
2025-08-07 13:35:52 -0400

    test: provide defaults for env vars

--- tests/mocks/github.ts
+++ tests/mocks/github.ts
@@ -128,8 +128,9 @@ async function getUser(request: Request) {
 }
 
 const passthroughGitHub =
-  !process.env.GITHUB_CLIENT_ID.startsWith('MOCK_') &&
-  process.env.NODE_ENV !== 'test';
+  !(process.env.GITHUB_CLIENT_ID ?? 'MOCK_GITHUB_CLIENT_ID').startsWith(
+    'MOCK_',
+  ) && process.env.NODE_ENV !== 'test';
 
 export const handlers: Array<HttpHandler> = [
   http.post(
