840cc48a097d34244d19693681187324fee2f882
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2025-08-08 10:26:27 -0400

    fix: streamline error display in TextareaField component by using conditional rendering

--- app/components/forms.tsx
+++ app/components/forms.tsx
@@ -141,9 +141,11 @@ export function TextareaField({
         aria-describedby={errorId}
         {...textareaProps}
       />
-      <div className="min-h-[32px] px-4 pb-3 pt-1">
-        {errorId ? <ErrorList id={errorId} errors={errors} /> : null}
-      </div>
+      {errorId && (
+        <div className="min-h-[32px] px-4 pb-3 pt-1">
+          <ErrorList id={errorId} errors={errors} />
+        </div>
+      )}
     </Stack>
   );
 }
