269ae64da5636bb91dcf3adf35d668bc3fb4c87c
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2025-08-12 17:59:50 -0400

    style: update logo colors in root component for improved theming

--- app/root.tsx
+++ app/root.tsx
@@ -280,10 +280,10 @@ const GroupsNav = () => {
 const Logo = () => {
   return (
     <Link to="/" className="group grid leading-snug">
-      <span className="font-light transition group-hover:-translate-x-1">
+      <span className="text-gift font-light transition group-hover:-translate-x-1">
         gift
       </span>
-      <span className="font-bold transition group-hover:translate-x-1">
+      <span className="text-pool font-bold transition group-hover:translate-x-1">
         pool
       </span>
     </Link>
