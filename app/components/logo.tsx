e9482720b03323ea7bb23b2fe53a3fb150c8c624
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2025-09-06 01:35:30 -0400

    refactor(logo): enhance logo component structure and styling
    - Updated the Logo component to use Flex for improved layout and spacing.
    - Replaced text spans with a combination of an image and Flex container for better visual alignment and responsiveness.
    - Adjusted transition effects for a smoother user experience on hover.

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,14 +1,20 @@
 import { Link } from '@remix-run/react';
+import { Flex } from './ui-kit';
 
 export const Logo = () => {
   return (
     <Link to="/" className="group grid text-2xl leading-snug sm:text-base">
-      <span className="font-light text-gift transition group-hover:-translate-x-1">
-        gift
-      </span>
-      <span className="font-bold text-pool transition group-hover:translate-x-1">
-        pool
-      </span>
+      <Flex gap={2}>
+        <img src="/img/logo.svg" alt="Gift Pool" className="h-8 w-8" />
+        <Flex>
+          <span className="text-body-md font-light text-gift transition group-hover:-translate-y-1">
+            gift
+          </span>
+          <span className="text-body-md font-bold text-pool transition group-hover:translate-y-1">
+            pool
+          </span>
+        </Flex>
+      </Flex>
     </Link>
   );
 };
