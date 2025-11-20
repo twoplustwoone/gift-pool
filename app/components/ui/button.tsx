28c6f7044e04dfe7fa27328c208f83aa9a1c2365
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2025-11-20 12:32:51 -0500

    feat: add extra small button size variant

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -39,6 +39,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-10 px-4 py-2',
         wide: 'px-24 py-5',
+        xs: 'h-8 px-2',
         sm: 'h-9 px-3',
         lg: 'h-11 px-8',
         pill: 'px-12 py-3 leading-3',
