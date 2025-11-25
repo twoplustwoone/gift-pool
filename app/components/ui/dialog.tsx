bebe146d39cc39fa854ea9d2b972c1634f9dcd58
Francisco <francisco.digiandomenico@gmail.com>
2025-11-25 14:01:21 -0500

    fix: refine mobile bottom sheet interactions

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -28,7 +28,7 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  DialogPrimitive.Content
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => {
   return (
     <DialogPortal>
