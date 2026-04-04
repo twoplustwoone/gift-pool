842de840259e52dcff1dd83894716ce6c63e8977
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2026-04-04 11:27:37 -0400

    fix(server): remove async promise executor

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -46,7 +46,7 @@ export default async function handleRequest(...args: DocRequestArgs) {
     : 'onShellReady';
 
   const nonce = loadContext.cspNonce?.toString() ?? '';
-  return new Promise(async (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     let didError = false;
     // NOTE: this timing will only include things that are rendered in the shell
     // and will not include suspended components and deferred loaders
